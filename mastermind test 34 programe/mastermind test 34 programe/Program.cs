e320b8882d4ac4be062dc1b6cae3c880af9869e9
Collin Guillaume <220053@CCM-Asty-Moulin.local>
2022-12-16 16:15:37 +0100

    edenszero

--- mastermind test 34 programe/mastermind test 34 programe/Program.cs
+++ mastermind test 34 programe/mastermind test 34 programe/Program.cs
@@ -16,75 +16,100 @@ namespace mastermind_test_34_programe
             bool cohesion;
             int compt;
             string repet;
+            compt = 0;
             do
             {
-                compt = 1;
                 cohesion = false;
-
+                coulDepart(tCodeSecret, alea, out tCodeSecret);
                 do
                 {
-                    coulDepart(tCodeSecret, alea, out tCodeSecret);
+                    compt = compt + 1;
+                    Console.WriteLine("essai N°"+compt);
                     coolPlayer(tCouleurJoueur, out tCouleurJoueur);
+                    pionblanc = 0;
+                    pionrouge = 0;
                     calcBonPlace(tCouleurJoueur, tCodeSecret, pionrouge, out pionrouge);
                     calcBonCoul(tCouleurJoueur, tCodeSecret, pionblanc, out pionblanc);
-                } while (cohesion == false || compt == 10);
-                Console.WriteLine("voulez vous recomencez");
-            } while (repet =="y");
-        } 
-            static void coulDepart(int[] tCodeSecret, Random alea, out int[] lol)
-            {
-
-                for (int tc = 0; tc < 4; tc++)
+                    if ((pionrouge == 4) && (pionblanc >= 4))
+                    {
+                        cohesion = true;
+                        compt = 10000;
+                    }
+                    else
+                    {
+                        Console.WriteLine("vous avez " + pionrouge + " couleurs a la bonne place et " + pionblanc + " bonne couleur");
+                        Console.WriteLine(" ");
+                        if(compt == 10)
+                        {
+                            cohesion = true;
+                        }
+                    }
+                } while (cohesion == false );
+                if (compt == 10000)
                 {
-                    tCodeSecret[tc] = alea.Next(0, 6);
+                    Console.WriteLine("vous avez gagné");
                 }
-                lol = tCodeSecret;
-            }
-            static void coolPlayer(int[] tCouleurJoueur, out int[] lol)
-            {
-                for (int compteur = 0; compteur < 4; compteur++)
+                else
                 {
-                    Console.WriteLine("entrez le numero de la couleur choisie n°" + compteur);
+                    Console.WriteLine("vous avez perdu");
+                }
+                Console.WriteLine("voulez vous recomencez oui(Y) non(N)");
+                repet = Console.ReadLine();
+            } while (repet == "Y");
+        }
+        static void coulDepart(int[] tCodeSecret, Random alea, out int[] lol)
+        {
+
+            for (int tc = 0; tc < 4; tc++)
+            {
+                tCodeSecret[tc] = alea.Next(1, 6);
+            }
+            lol = tCodeSecret;
+        }
+        static void coolPlayer(int[] tCouleurJoueur, out int[] lol)
+        {
+            for (int compteur = 0; compteur < 4; compteur++)
+            {
+                Console.WriteLine("entrez le numero de la couleur choisie n°" + compteur );
+                Console.WriteLine("(1 rouge, 2 jaune, 3 bleu,4 orange, 5 vert, 6 violet)");
+                tCouleurJoueur[compteur] = int.Parse(Console.ReadLine());
+
+                while (tCouleurJoueur[compteur] <= 0 || tCouleurJoueur[compteur] > 7)
+                {
+
+                    Console.WriteLine("ERROR NUMERO DE COULEUR NON CORRECT");
+                    Console.WriteLine("entrez le numero de la couleur choisie n°" + compteur );
                     Console.WriteLine("(1 rouge, 2 jaune, 3 bleu,4 orange, 5 vert, 6 violet)");
                     tCouleurJoueur[compteur] = int.Parse(Console.ReadLine());
-                    Console.WriteLine(tCouleurJoueur[compteur]);
-
-                    while (tCouleurJoueur[compteur] < 0 || tCouleurJoueur[compteur] > 7)
-                    {
-
-                        Console.WriteLine("ERROR NUMERO DE COULEUR NON CORRECT");
-                        Console.WriteLine("entrez le numero de la couleur choisie n°" + compteur);
-                        Console.WriteLine("(1 rouge, 2 jaune, 3 bleu,4 orange, 5 vert, 6 violet)");
-                        tCouleurJoueur[compteur] = int.Parse(Console.ReadLine());
-                    }
-
                 }
-                lol = tCouleurJoueur;
+
             }
-            static void calcBonPlace(int[] tCouleurJoueur, int[] tCodeSecret, int pion, out int pionrouge)
+            lol = tCouleurJoueur;
+        }
+        static void calcBonPlace(int[] tCouleurJoueur, int[] tCodeSecret, int pion, out int pionrouge)
+        {
+            for (int compteur = 0; compteur < 4; compteur++)
             {
-                for (int compteur = 0; compteur < 4;compteur++)
+                if (tCouleurJoueur[compteur] == tCodeSecret[compteur])
                 {
-                    if (tCouleurJoueur[compteur] == tCodeSecret[compteur])
+                    pion = pion + 1;
+                }
+            }
+            pionrouge = pion;
+        }
+        static void calcBonCoul(int[] tCouleurJoueur, int[] tCodeSecret, int pion, out int pionblanc)
+        {
+            for (int compteur = 0; compteur < 4; compteur++)
+            {
+                for (int compteur2 = 0; compteur2 < 4; compteur2++)
+                {
+                    if (tCodeSecret[compteur] == tCouleurJoueur[compteur2])
                     {
                         pion = pion + 1;
                     }
                 }
-                pionrouge = pion;
             }
-            static void calcBonCoul(int[] tCouleurJoueur, int[] tCodeSecret, int pion, out int pionblanc)
-            {
-                for (int compteur = 0; compteur < 4; compteur++)
-                {
-                    for (int compteur2 = 0; compteur2 < 4; compteur2++)
-                    {
-                        if (tCodeSecret[compteur] == tCouleurJoueur[compteur2])
-                        {
-                            pion = pion + 1;
-                        }
-                    }
-                }
-                pionblanc = pion;
-            }
+            pionblanc = pion;
+        }
     }
 }
